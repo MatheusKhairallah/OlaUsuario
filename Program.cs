95f7c86eab1ee21b2848408c05fde38ae2038249
matheus <playermaster10like@gmail.com>
2020-03-13 20:41:34 -0300

    programa c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor, digite seu nome: ");
+            string nome=Console.ReadLine();
+
+            string mensagemNome="Obrigado, " +nome+ "!";
+
+            Console.WriteLine(mensagemNome);
+        }
+    }
+}
